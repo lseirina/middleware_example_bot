2d6211e2805cd7c06ce27d985c7cd965eb9ce1ae
root <lseirina87@gmail.com>
2024-05-03 10:59:50 +0300

    Added oher handler

---
+++ handlers/other.py
@@ -0,0 +1,21 @@
+import logging
+
+from aiogram import Router
+from aiogram.types import Message
+from filters.filters import MyTrueFilter
+from lexicon.lexicon import LEXICON_RU
+
+
+logger = logging.getLogger(__name__)
+
+other_router = Router()
+
+
+@other_router.message(MyTrueFilter)
+async def send_echo(message: Message):
+    logger.debug('Entered echo-handler')
+    try:
+        await message.send_copy(chat_id=message.chat_id)
+    except TypeError:
+        await message.answer(text=LEXICON_RU['no_echo'])
+    logger.debug('Left echo-handler')
