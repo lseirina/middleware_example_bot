1f8c4a81152058457ab4ba74c4e1a3da6ef3bc69
root <lseirina87@gmail.com>
2024-05-10 21:30:29 +0300

    Added middlewares in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-
+from aiogram.fsm.state import StatesGroup
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers.other import other_router
@@ -36,6 +36,11 @@ async def main() -> None:
     dp.include_router(other_router)
 
     dp.update.outer_middleware(FirstOuterMiddleware())
+    user_router.callback_query.outer_middleware(SecondOuterMiddleware())
+    other_router.message.outer_middleware(ThirdOuterMiddleware())
+    user_router.message.middleware(FirstInnerMiddleware())
+    user_router.callback_query.middleware(SecondInnerMiddleware())
+    other_router.message.middleware(ThirdInnerMiddleware())
 
     await dp.start_polling(bot)
 
--- middlewares/inner.py
+++ middlewares/inner.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 class FirstInnerMiddleware(BaseMiddleware):
-    async def __cal__(
+    async def __call__(
         self,
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
@@ -29,7 +29,7 @@ class FirstInnerMiddleware(BaseMiddleware):
 
 
 class SecondInnerMiddleware(BaseMiddleware):
-    async def __cal__(
+    async def __call__(
         self,
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
