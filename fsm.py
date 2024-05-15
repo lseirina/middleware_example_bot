35cdd20387c41523122f562e040ae76e57057039
root <lseirina87@gmail.com>
2024-05-15 12:56:18 +0300

    Added state fill_name and fill_age

--- fsm.py
+++ fsm.py
@@ -49,3 +49,44 @@ async def process_cancel_command_state(message: Message, state: FSMContext):
         text='Everything is deleted.'
     )
     state.clear()
+
+
+@dp.message(Command(commands='fillform'), StateFilter(default_state))
+async def process_fillform_command(message: Message, state: FSMContext):
+    await message.answer('Please, write your name')
+    await state.set_state(FSMFillForm.fill_name)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_name), F.text.isalpha())
+async def process_name_sent(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('And now write your age')
+    await state.set_state(FSMFillForm.fill_age)
+
+
+@dp.message(StateFilter(FSMFillForm.fill_name))
+async def process_warn_name(message: Message):
+    await message.answer(
+        text='This is does not look like a name.'
+    )
+
+
+@dp.message(StateFilter(FSMFillForm.fill_age),
+            lambda x: x.text.isdigit() and 4 <= int(x.text) <= 120)
+async def process_age_send(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    f_button = InlineKeyboardButton(
+        text='female',
+        calback_data='female'
+    )
+    m_button = InlineKeyboardButton(
+        text='man',
+        callback_data='man'
+    )
+    keyboard: list[list[InlineKeyboardButton]] = [[f_button, m_button]]
+    markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    await message.answer(
+        text='Choose your gender',
+        markup=markup
+    )
+    await state.set_state(FSMFillForm.fill_gender)
